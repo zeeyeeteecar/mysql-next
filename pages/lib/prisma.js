c7ef0679cc1229cc161bca0a0169db4a1617a2dc
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-10-10 10:10:29 -0700

    4

--- pages/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const Prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV === 'development') global.prisma = Prisma
-
-export default Prisma
