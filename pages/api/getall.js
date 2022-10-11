f90cda1e069911cc2479398ac2e1ef8d73dd5695
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-10-10 18:02:41 -0700

    9

--- pages/api/getall.js
+++ pages/api/getall.js
@@ -1,11 +1,11 @@
-import prisma from "../lib/prisma";
+import Prisma from "../components/prisma";
 //import { PrismaClient } from "@prisma/client";
 //const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
   //const prisma = new PrismaClient();
 
-  const result = await prisma.User.findMany({
+  const result = await Prisma.User.findMany({
     orderBy: [
       {
         id: "asc",
---
+++ pages/components/prisma.js
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+let Prisma = new PrismaClient();
+
+export default Prisma;
